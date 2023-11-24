53500b63057030ffcba2599e1f15815deb479268
haphong463 <haphong463@gmail.com>
2023-11-24 20:45:58 +0700

    ok

---
+++ client/src/containers/admin/Home.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { Sidebar } from "../../components/Sidebar";
+import { Box, Card, CardContent, Grid, Stack, Typography } from "@mui/material";
+import Navbar from "../../components/Navbar";
+import "../../assets/styles/Dashboard.css";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+export const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Box height={70} />
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Stack spacing={2} direction="row">
+                <Card sx={{ maxWidth: 49 + "%", height: 140 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      $500.00
+                    </Typography>
+                  </CardContent>
+                </Card>
+                <Card sx={{ maxWidth: 49 + "%", height: 140 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
+            <Grid item xs={4}>
+              <Stack spacing={2}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <Stack spacing={2} direction="row">
+                    <div className="iconstyle">
+                      <StorefrontIcon />
+                    </div>
+                    <div className="paddingall">
+                      <span className="priceTitle">$203K</span>
+                      <br />
+                      <span className="priceSubTitle">Total Income</span>
+                    </div>
+                  </Stack>
+                </Card>
+                <Card sx={{ maxWidth: 345 }}>
+                  <Stack spacing={2} direction="row">
+                    <div className="iconstyle">
+                      <StorefrontIcon />
+                    </div>
+                    <div className="paddingall">
+                      <span className="priceTitle">$203K</span>
+                      <br />
+                      <span className="priceSubTitle">Total Income</span>
+                    </div>
+                  </Stack>
+                </Card>
+              </Stack>
+            </Grid>
+          </Grid>
+          <Box height={20} />
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <Card sx={{ height: 60 + "vh" }}>
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card sx={{ height: 60 + "vh" }}>
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </>
+  );
+};
